200621b6a2d3ccd7f291c41a4a589e1c4fcbdba9
alenasaz <aLeNka_saZaNova@mail.ru>
2018-10-06 17:44:16 +0300

    11 slide

--- 11sl.py
+++ 11sl.py
@@ -1,24 +1,41 @@
-def additional1(number,count=1):
+def money1(a,x,b,y,z):
+    i=0
+    j=0
+    if a*x+b*y<=z:
+        print("Нельзя")
+    elif a*x==z:
+        print("Можно")
+    elif b*y==z:
+        print("Можно")
+    else:
+        i+=1
+        j+=1
+        for i in range(a):
+            for j in range(b):
+                if i*x+j*y==z:
+                    print("Можно")
+money1(5,4,6,7,19)
+def additional2(number,count=1):
     number//=10
     while number>0:
         number//=10
         count+=1
     print(count)
-additional1(12)
+additional2(12)
 
 #палиндром-она одинаково читается слева направо и справа налево
-def palindrom2(stroka):
+def palindrom3(stroka):
     a=stroka[::-1]
     if stroka==a:
         print("Да, является")
     else:
         print("Нет, не является")
-palindrom2('Капуста')
+palindrom3('Капуста')
 
-def findwoef3(str, strinword, newstr):
+def findwoef4(str, strinword, newstr):
     length=len(strinword)
     while str.find(strinword)>0:
         indexofword=str.find(strinword)
         str=str[:indexofword]+newstr+str[indexofword+length:]
     print(str)
-findwoef3("Привет завтра вчера пока сегодня","сегодня","нет")
+findwoef4("Привет завтра вчера пока сегодня","сегодня","нет")
