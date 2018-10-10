2023c29169c9cd46f53396cb364f26810a537228
alenasaz <aLeNka_saZaNova@mail.ru>
2018-10-10 21:37:12 +0300

    10.10.2018

--- 8sl.py
+++ 8sl.py
@@ -5,8 +5,6 @@ def run1(a=2,i=0):
     print("Days",i)
 run1()
 
-
-
 def run2():
   sum=4
   num=1
@@ -22,7 +20,7 @@ def run2():
                   IsSimple = False
                   break
       sum += num
-      print(sum, num)
+      #print(sum, num)
       day += 1
   print("Days", day)
 run2()
@@ -46,11 +44,18 @@ def run3(firstday=10,i=1):
         i+=1
     print(firstday)
 run3()
-
+#day1-кол-во киломметров в день
+#all_days-весь путь
 def run4(day1=10,counter_of_days=1,all_days=0):
-    while day1<20 and all_days<100:
+    while day1<=21:
         day1+=day1*0.1
-        all_days=+day1
         counter_of_days+=1
     print(counter_of_days)
 run4()
+def run5(day1=10,counter_of_days=1,all_days=10):
+    while all_days<101:
+        day1+=day1*0.1
+        all_days=+day1
+        counter_of_days+=1
+    print(counter_of_days-1)
+run5()
